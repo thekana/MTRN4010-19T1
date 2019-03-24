1eb24e2d9c64e78a265f2c060a40dd9cf79484b4
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-03-24 19:54:51 +1100

    Task C frequency fixed

--- Project02_release[2019]/Project_2_A_B.m
+++ Project02_release[2019]/Project_2_A_B.m
@@ -1,9 +1,10 @@
 load('DataForProject02/IMU_dataC.mat');
 load('DataForProject02/Speed_dataC.mat');
+load('DataForProject02/Laser__2C.mat');
 
 % remove bias
 time = double(IMU.times-IMU.times(1))/10000;
-%timeV = double(Vel.times-Vel.times(1))/10000;
+Laser_time = double(dataL.times-dataL.times(1))/10000;
 
 yaw = -IMU.DATAf(6,:); % negate w(K)
 
@@ -22,11 +23,26 @@ thetaK(1) = pi/2;
 xK(1) = 0;
 yK(1) = 0;
 
+thetaKL = zeros(1,length(Laser_time));
+xKL = zeros(1,length(Laser_time));
+yKL = zeros(1,length(Laser_time));
+thetaKL(1) = pi/2;
+xKL(1) = 0;
+yKL(1) = 0;
+
+j = 2;
 for i = 2:L
     dt = time(i)-time(i-1); % find dt
     thetaK(i) = thetaK(i-1) + dt * yawC(i-1); % euler approx
     xK(i) = xK(i-1) + dt * Vel.speeds(i-1)*cos(thetaK(i));
     yK(i) = yK(i-1) + dt * Vel.speeds(i-1)*sin(thetaK(i));
+    if (j <= length(Laser_time) && Laser_time(j) - time(i-1)< dt)
+        dtL = Laser_time(j) - time(i-1);
+        thetaKL(j) = thetaK(i-1) + dtL * yawC(i-1); % euler approx
+        xKL(j) = xK(i-1) + dtL * Vel.speeds(i-1)*cos(thetaK(i));
+        yKL(j) = yK(i-1) + dtL * Vel.speeds(i-1)*sin(thetaK(i));
+        j = j + 1;
+    end
 end
 
 % convert from radian to degree
@@ -35,3 +51,7 @@ figure();
 plot(time,thetaK);
 figure();
 plot(xK,yK);
+figure();
+plot(Laser_time,thetaKL);
+figure();
+plot(xKL,yKL);
