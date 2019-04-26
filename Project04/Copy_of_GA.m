cb546559aad4e442c20a4772081c13db7612b1fe
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-04-26 10:56:37 +1000

    Should be good enough

--- Project04/Copy_of_GA.m
+++ Project04/Copy_of_GA.m
@@ -1,8 +1,8 @@
 global N_G N_C P_m P_c
 N_G = 1000; %Number of generations
 N_C = 300; %Population // Number of Chromosomes
-P_m = 0.99; %Probablity of mutation
-P_c = 0.50; %Probablity of crossover
+P_m = 0.80; %Probablity of mutation
+P_c = 0.70; %Probablity of crossover
 map = zeros(3,11);
 history = zeros(2,N_G);
 for i = 1:length(map)
@@ -13,6 +13,9 @@ end
 startPoint = -40 + (40+40)*rand([2,1]);
 endPoint = -40 + (40+40)*rand([2,1]);
 %disp(map);
+handle = plot(0,0);
+text(startPoint(1),startPoint(2),'Start');
+text(endPoint(1),endPoint(2),'End');
 population = zeros(N_C,11);
 for i = 1:N_C
    noDup = false;
@@ -33,40 +36,35 @@ for i = 1:N_G
     disp(i)
     fitness = calcFitness(startPoint,endPoint,map,population);
     history(:,i) = [i;min(fitness(:,1))];
+    [~,eliteIndex] = min(fitness(:,1));
+    elite = population(eliteIndex,:);
+    tempMap = map(:,elite);
+    x = [startPoint(1),tempMap(2,:),endPoint(1)];
+    y = [startPoint(2),tempMap(3,:),endPoint(2)];
+    set(handle,'xdata',x,'ydata',y,'marker','*');
+    pause(0.001);
     % disp(fitness);
     meanWeight = mean(fitness(:,2));
     fitnessScore = fitness(:,2)/meanWeight;
-    % remove fitnessScore < 1
-    parents = population(fitnessScore>=1.0,:);
-    % selection completed
-    % Shuffle
-    parents = parents(randperm(size(parents, 1)), :);
-    
+    probArray = fitnessScore/sum(fitnessScore);
+    %sum(probArray)
     %% Cross Over
-    populationSize = size(population,1);
     nextGen = [];
     for j = 1:N_C
-        %disp(j);
-        index1 = 0;
-        index2 = 0;
         if rand()>P_c
            crossOverPoint = randi([2,10],1,1);
-           while index1 == index2
-               index1 = randi([1,size(parents,1)],1,1);
-               index2 = randi([1,size(parents,1)],1,1);
-           end
-           parent1 = parents(index1,:);
-           parent2 = parents(index2,:);
-           child1 = [parent2(1:crossOverPoint),parent1(crossOverPoint+1:11)];
-           child2 = [parent1(1:crossOverPoint),parent2(crossOverPoint+1:11)];
-           nextGen = [nextGen;child1;child2];
+           parent = randsample(length(probArray),2,true,probArray);
+           parentA = population(parent(1),:);
+           parentB = population(parent(2),:);
+           child1 = [parentB(1:crossOverPoint),parentA(crossOverPoint+1:11)];
+           child2 = [parentA(1:crossOverPoint),parentB(crossOverPoint+1:11)];
+           nextGen = [nextGen;child1];
+           nextGen = [nextGen;child2];
         end
-%        if size(population,1) >= N_C
-%            break;
-%        end
     end
+    population = nextGen;
     %% mutation
-    populationSize = size(nextGen,1);
+    populationSize = size(population,1);
     for j = 1:populationSize
         if rand()>(P_m)
             index1 = 0;
@@ -75,32 +73,29 @@ for i = 1:N_G
                index1 = randi([1,11],1,1);
                index2 = randi([1,11],1,1);
             end
-            temp = nextGen(j,index1);
-            nextGen(j,index1) = nextGen(j,index2);
-            nextGen(j,index2) = temp;
+            temp = population(j,index1);
+            population(j,index1) = population(j,index2);
+            population(j,index2) = temp;
         end
         % remove duplicate
-        diff = setdiff(map(1,:),nextGen(j,:));
+        diff = setdiff(map(1,:),population(j,:));
         if diff
-            [~,ia,~] = unique(nextGen(j,:),'first');     %unique indices
+            [~,ia,~] = unique(population(j,:),'first');     %unique indices
             dupInd = setdiff(map(1,:),ia);                  %dup indices
             for k=1:length(dupInd)
-                nextGen(j,dupInd(k)) = diff(k);
+                population(j,dupInd(k)) = diff(k);
             end
         end
-        diff = setdiff(map(1,:),nextGen(j,:));
+        diff = setdiff(map(1,:),population(j,:));
         assert(isempty(diff));
     end
     %% Elitism
-    if size(parents,1) < 10
-        nextGen = [nextGen;parents];
-    else
-        nextGen = [nextGen;parents(1:10,:)];
-    end
-    population = nextGen;
+    population(randsample(populationSize,1),:) = elite;
 end
-
+figure()
 plot(history(1,:),history(2,:));
+xlabel('Generation');
+ylabel('Distance');
 
 function fitness = calcFitness(startP,endP,map,population)
 
