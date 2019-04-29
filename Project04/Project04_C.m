3a508262fa8a5a6f6fc5434b7f61873db4523455
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-04-29 17:55:13 +1000

    Part C finished?

--- Project04/Project04_C.m
+++ Project04/Project04_C.m
@@ -1,13 +1,13 @@
 function []=Project04_C()
 % sim car in random movement
 
-clc; clear all; close all; dbstop if error;
+clc; clear ; close all; dbstop if error;
 set(0,'defaultaxesfontname','times new roman');
 field.range=50;
 time.dt=1; time.T=500;
 %% Initialization
 
-[fig]=FigureNew(field);
+[fig] = FigureNew(field);
 carPosition = [(rand-0.5)*field.range,(rand-0.5)*field.range,wrapToPi(rand*2*pi)];
 [car] = CarNew(carPosition,'r');
 [car] = CarNow(car,time,0,0);
@@ -26,7 +26,7 @@ fis_ang=readfis('MTRN4010_ang.fis');
 %% PSO parameters
 
 PSO.DLB=0; PSO.DUB=30; %distance lower bound and upper bound
-PSO.D=1; PSO.G=1; PSO.N=3;% particle dimension generations, particles
+PSO.D=1; PSO.G=20; PSO.N=20;% particle dimension generations, particles
 PSO.V=rand(PSO.D,PSO.N);% initial PSO particle velocity
 PSO.Gbest=[]; PSO.gbest=realmax;% PSO gbest
 PSO.Pbest=[]; PSO.pbest=ones(1,PSO.N)*realmax;% PSO pbest
@@ -37,7 +37,8 @@ PSO.X=rand(PSO.D,PSO.N);
 PSO.BND=[PSO.DLB PSO.DUB];% PSO bound
 
 PSO.X(1,:)=PSO.BND(1,1)+PSO.X(1,:)*diff(PSO.BND(1,:)); % not sure what this does yet
-history = zeros(1,PSO.G);
+history = zeros(1,PSO.G); % buffer for history
+
 %% PSO main loop
 carInit = car;
 for g=1:PSO.G
@@ -47,14 +48,13 @@ for g=1:PSO.G
     for n=1:PSO.N % number of particles
         car = carInit;
         d = PSO.X(n);  % pick one particle
-        virtualPosition = [targetPosition(1)-d*cos(targetHeading),targetPosition(2)-d*sin(targetHeading),targetHeading];
-        [virtual] = CarNew(virtualPosition,'k');
+        [virtual] = virtualNew(targetPosition,d);
         virtualVel = calculateDistance(virtual,target)/time.T;
         for t=0:time.dt:time.T
             vel = evalfis(calculateDistance(car,virtual),fis_vel);
             omega = evalfis(calculateAngle(car,virtual),fis_ang);
             [virtual] = CarNow(virtual,time,virtualVel,0);
-            [car]=CarNow(car,time,vel,omega);
+            [car] = CarNow(car,time,vel,omega);
         end
         err = [car.x-target.x,car.y-target.y,wrapToPi(car.q-target.q)];
         fit(n) = sqrt(sum(err.^2));
@@ -82,10 +82,8 @@ for g=1:PSO.G
     PSO.X(1,z)=PSO.BND(1,1)+rand(1,length(z))*diff(PSO.BND(1,:));
     z=find(PSO.X(1,:)>PSO.BND(1,2));
     PSO.X(1,z)=PSO.BND(1,1)+rand(1,length(z))*diff(PSO.BND(1,:));
-    disp(PSO.X);
 end
-% figure(handle)
-% set(handle,'xdata',1:PSO.G,'ydata',history);
+%% Error vs Generation graph
 figure();
 plot(1:PSO.G,history);
 xlabel('Generation');
@@ -93,18 +91,18 @@ ylabel('Error');
 title('POS');
 
 %% Showing the optimized distance
-[figNew]=FigureNew(field);
-[car] = CarNew(carPosition,'r');
-[car] = CarNow(car,time,0,0);
-[target] = CarNew(targetPosition,'b');
-[target] = CarNow(target,time,0,0);
-d = PSO.Gbest(1);
+figure(fig);        % set to current axis
+d = PSO.Gbest(1);   % get best distance
+car = carInit;      % reset car position
+% Generate Virtual target
 virtualPosition = [targetPosition(1)-d*cos(targetHeading),targetPosition(2)-d*sin(targetHeading),targetHeading];
 [virtual] = CarNew(virtualPosition,'k');
 [virtual] = CarNow(virtual,time,0,0);
-[virtual] = CarShow(virtual,0);
-[car] = CarShow(car,0);
-[target] = CarShow(target,0);
+
+% plot all objects on fig
+[virtual] = CarShow(fig,virtual,0);
+[car] = CarShow(fig,car,0);
+[target] = CarShow(fig,target,0);
 
 %% Calculate the velocity at which the virtual target moves to real target
 virtualVel = calculateDistance(virtual,target)/time.T;
@@ -114,10 +112,10 @@ for t=0:time.dt:time.T
     omega = evalfis(calculateAngle(car,virtual),fis_ang);
     
     [virtual] = CarNow(virtual,time,virtualVel,0);
-    [virtual] = CarShow(virtual,t);
+    [virtual] = CarShow(fig,virtual,t);
     
     [car]=CarNow(car,time,vel,omega);
-    [car]=CarShow(car,t);
+    [car]=CarShow(fig,car,t);
 end
 
 end
@@ -129,51 +127,37 @@ xlabel('x-direction'); ylabel('y-direction');
 end
 
 function [car]=CarNew(position,color)
-car.x=position(1); car.y=position(2); car.q=position(3);
-car.trace=[car.x; car.y; car.q];
-car.shape=[ 2 0; 1 1; -1 1; -1 -1; 1 -1; 2 0]';
-car.hdL.shape=plot(car.shape(1,:),car.shape(2,:),'color',color,'linewidth',2);
-car.hdL.trace=plot(car.trace(1,:),car.trace(2,:),'color',[0 0.66 0]);
+    car.x=position(1); car.y=position(2); car.q=position(3);
+    car.trace=[car.x; car.y; car.q];
+    car.shape=[ 2 0; 1 1; -1 1; -1 -1; 1 -1; 2 0]';
+    car.hdL.shape=plot(car.shape(1,:),car.shape(2,:),'color',color,'linewidth',2);
+    car.hdL.trace=plot(car.trace(1,:),car.trace(2,:),'color',[0 0.66 0]);
 end
 
 function [car]=CarNow(car,time,v,w)
-car.x=car.x+time.dt*v*cos(car.q);
-car.y=car.y+time.dt*v*sin(car.q);
-car.q=car.q+time.dt*w;
-car.q=wrapToPi(car.q);
-car.trace(:,end+1)=[car.x; car.y; car.q];
+    car.x=car.x+time.dt*v*cos(car.q);
+    car.y=car.y+time.dt*v*sin(car.q);
+    car.q=car.q+time.dt*w;
+    car.q=wrapToPi(car.q);
+    car.trace(:,end+1)=[car.x; car.y; car.q];
 end
 
-function [car]=CarShow(car,t)
-ax=axis;
-Rz=[  cos(car.q) -sin(car.q); 
-      sin(car.q)  cos(car.q)];
-shape=Rz*car.shape+repmat([car.x;car.y],1,6);
-set(car.hdL.shape,'xdata',shape(1,:),'ydata',shape(2,:)); 
-set(car.hdL.trace,'xdata',car.trace(1,:),'ydata',car.trace(2,:));
-axis(ax); title(sprintf('Time %d',t)); pause(0.001);
+function [car]=CarShow(fig,car,t)
+    ax=axis;
+    Rz=[  cos(car.q) -sin(car.q); 
+          sin(car.q)  cos(car.q)];
+    shape=Rz*car.shape+repmat([car.x;car.y],1,6);
+    set(car.hdL.shape,'xdata',shape(1,:),'ydata',shape(2,:)); 
+    set(car.hdL.trace,'xdata',car.trace(1,:),'ydata',car.trace(2,:));
+    axis(ax); title(sprintf('Time %d',t)); pause(0.001);
 end
 
-% function [target] = TargetNew(field)
-% target.x=(rand-0.5)*field.range;
-% target.y=(rand-0.5)*field.range;
-% target.q=AngleWrap(rand*2*pi);
-% target.shape=[ 2 0; 1 1; -1 1; -1 -1; 1 -1; 2 0]';
-% target.hdL.shape=plot(target.shape(1,:),target.shape(2,:),'color','r','linewidth',2);
-% Rz=[ cos(target.q) -sin(target.q);
-% sin(target.q) cos(target.q)];
-% shape=Rz*target.shape+repmat([target.x;target.y],1,6);
-% set(target.hdL.shape,'xdata',shape(1,:),'ydata',shape(2,:));
-% end
-% 
-% function [q]=AngleWrap(q)
-% while q<pi
-%     q=q+2*pi;
-% end
-% while q>pi
-%     q=q-2*pi;
-% end
-% end
+function [virtual]=virtualNew(target,d)
+    % This function omits the plotting
+    % to be used in POS loop
+    virtual.x=target(1)-d*cos(target(3)); virtual.y=target(2)-d*sin(target(3)); virtual.q=target(3);
+    virtual.trace=[virtual.x; virtual.y; virtual.q];
+end
 
 function distance = calculateDistance(src,dest)
     distance = sqrt((src.x-dest.x)^2+(src.y-dest.y)^2);
